788cbb1c3e54ae200b4382488947e4899b4c30f9
dkmqflx <dkmqflx@gmail.com>
2024-03-23 23:37:33 +0900

    chore: FSD 기반 기본 폴더 구조 세팅

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [0, 'always', ['feat', 'fix', 'chore', 'build', 'style', 'test']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'build', 'style', 'test']],
+    'subject-case': [2, 'always', ['lower-case', 'start-case']],
   },
 };
