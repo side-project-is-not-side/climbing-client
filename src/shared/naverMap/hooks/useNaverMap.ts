ad872831ba414758c2cbabcce5ee764da89b13c0
yousunzoo <dpfvmffjqm@gmail.com>
2024-03-28 01:09:42 +0900

    feat: 현재 위치로 이동 버튼 구현

--- src/shared/naverMap/hooks/useNaverMap.ts
+++ src/shared/naverMap/hooks/useNaverMap.ts
@@ -1,8 +1,8 @@
-'use client';
+import { useEffect, useRef, useState } from 'react';
 
-import { useRef, useState } from 'react';
+import { useGeoLocation } from './useGeolocation';
 
-import { INITIAL_CENTER, INITIAL_ZOOM, MIN_ZOOM } from '@/shared/naverMap/constants';
+import { INITIAL_CENTER, ZOOM_LEVEL } from '@/shared/naverMap/constants';
 import { Coordinates, NaverMapOptions, TNaverMap } from '@/shared/naverMap/types';
 
 type Props = {
@@ -11,17 +11,22 @@ type Props = {
   initialZoom?: number;
   minZoom?: number;
   onLoad?: (map: TNaverMap) => void;
+  geolocationEnabled: boolean;
 };
 
 const useNaverMap = ({
   mapId = 'map',
-  initialZoom = INITIAL_ZOOM,
+  initialZoom = ZOOM_LEVEL.거리,
   initialCenter = INITIAL_CENTER,
-  minZoom = MIN_ZOOM,
+  minZoom = ZOOM_LEVEL.시도,
+  geolocationEnabled,
   onLoad,
 }: Props) => {
   const mapElementRef = useRef<HTMLDivElement | null>(null);
   const [map, setMap] = useState<TNaverMap | null>(null);
+
+  const { location, error, reload } = useGeoLocation(geolocationEnabled);
+  const isGeolocationLoading = location === undefined && !error;
 
   const initializeMap = () => {
     const mapOptions: NaverMapOptions = {
@@ -31,7 +36,7 @@ const useNaverMap = ({
       scaleControl: false,
       mapDataControl: false,
       logoControlOptions: {
-        position: naver.maps.Position.BOTTOM_LEFT,
+        position: naver.maps.Position.TOP_LEFT,
       },
     };
 
@@ -43,6 +48,39 @@ const useNaverMap = ({
     }
   };
 
-  return { mapElementRef, initializeMap, map };
+  const handleMoveToCurrentLocation = () => {
+    if (!location) return;
+
+    const mapOptions: NaverMapOptions = {
+      center: new window.naver.maps.LatLng(...location),
+      zoom: initialZoom,
+      minZoom,
+      scaleControl: false,
+      mapDataControl: false,
+      logoControlOptions: {
+        position: naver.maps.Position.BOTTOM_LEFT,
+      },
+    };
+
+    const newMap = new window.naver.maps.Map(mapId, mapOptions);
+    setMap(newMap);
+  };
+
+  const onCurrentLocationChanged = () => {
+    reload();
+  };
+
+  useEffect(() => {
+    handleMoveToCurrentLocation();
+  }, [location]);
+
+  return {
+    mapElementRef,
+    initializeMap,
+    map,
+    currentLocation: location,
+    onCurrentLocationChanged,
+    isGeolocationLoading,
+  };
 };
 export default useNaverMap;
