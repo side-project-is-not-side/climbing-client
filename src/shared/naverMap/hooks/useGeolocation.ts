876f439c3c7d6da4e24660c5032a43db0a715886
yousunzoo <dpfvmffjqm@gmail.com>
2024-03-28 00:02:15 +0900

    feat: 현재 위치 가져오는 커스텀 훅

---
+++ src/shared/naverMap/hooks/useGeolocation.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+import { Coordinates } from '../types';
+
+type GeolocationOptions = {
+  enableHighAccuracy: boolean;
+  timeout: number;
+  maximumAge: number;
+};
+
+export const useGeoLocation = (isActive: boolean, options?: GeolocationOptions) => {
+  const [location, setLocation] = useState<Coordinates>();
+  const [error, setError] = useState('');
+
+  const onSuccess = (position: GeolocationPosition) => {
+    const { latitude, longitude } = position.coords;
+
+    setLocation([latitude, longitude]);
+  };
+
+  const onError = (error: GeolocationPositionError) => {
+    setError(error.message);
+  };
+
+  useEffect(() => {
+    const { geolocation } = navigator;
+
+    if (!geolocation || !isActive) {
+      setError('not supported');
+      return;
+    }
+
+    geolocation.getCurrentPosition(onSuccess, onError, options);
+  }, [options, isActive]);
+
+  return { location, error };
+};
