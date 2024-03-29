fe0b9a2617e10d2f91a260f5a37822becaf58254
dkmqflx <dkmqflx@gmail.com>
2024-03-29 22:31:59 +0900

    feat: cache 옵션 설정

--- src/entities/main/api/getRecommendations.ts
+++ src/entities/main/api/getRecommendations.ts
@@ -12,7 +12,9 @@ export type RecommendationGymList = {
 };
 
 export const getRecommendationGym = async (): Promise<RecommendationGymList[]> => {
-  const res = await fetch(`https://${process.env.NEXT_PUBLIC_API_HOST}/api/bouldering-gym/recommendation`);
+  const res = await fetch(`https://${process.env.NEXT_PUBLIC_API_HOST}/api/bouldering-gym/recommendation`, {
+    cache: 'no-cache',
+  });
 
   if (!res.ok) {
     throw new Error('Failed to fetch data');
