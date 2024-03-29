834bf8dca229e2e18e965752cb3c743cc64fae08
yousunzoo <dpfvmffjqm@gmail.com>
2024-03-30 00:28:30 +0900

    fix: 업데이트 날짜 한글화

--- src/shared/ui/SectorUpdateInfo.tsx
+++ src/shared/ui/SectorUpdateInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { GrayStone, RedStone } from '../icons/components';
+import dayjs from 'dayjs';
 
 import { TSectorUpdateInfo } from '@/entities/map/api/types';
 
@@ -15,7 +16,9 @@ const SectorUpdateInfo = ({
   return (
     <div className={`flex gap-[10px] ${type === 'upcoming' ? 'text-primary-400 ' : 'text-neutral-400'} text-[14px]`}>
       {type == 'upcoming' ? <RedStone width={20} height={20} /> : <GrayStone width={20} height={20} />}
-      <p>{date}</p>
+      <p>
+        {type === 'lastUpdated' && '마지막 업데이트'} {dayjs(date).format('M월 DD일 a h:mm')}
+      </p>
       <span>&middot;</span>
       <p>{name}</p>
     </div>
