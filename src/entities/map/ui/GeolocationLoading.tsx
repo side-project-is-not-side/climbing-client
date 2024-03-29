cc5679aa129707b025571931ad0a82f69bd36127
yousunzoo <dpfvmffjqm@gmail.com>
2024-03-29 23:41:16 +0900

    fix: 로딩 컴포넌트 뜨지 않는 현상 수정

--- src/entities/map/ui/GeolocationLoading.tsx
+++ src/entities/map/ui/GeolocationLoading.tsx
@@ -1,26 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import Image from 'next/image';
 
-import { createPortal } from 'react-dom';
-
 function GeolocationLoading({ visible }: { visible: boolean }) {
-  const [element, setElement] = useState<HTMLElement | null>(null);
-
-  useEffect(() => {
-    setElement(document.body);
-  }, []);
-
-  if (!element) return <></>;
-
-  return createPortal(
+  return (
     <section
       className={`fixed z-[101] bg-[rgba(0,0,0,0.98)] w-full h-full flex-col justify-center items-center ${visible ? 'flex' : 'hidden'}`}
     >
       <Image src={'/images/spinner.png'} className="animate-spin" width={50} height={50} alt="위치 정보 불러오는 중" />
       <p className="text-neutral-400 text-[14px] leading-5">암장 정보를 가져오는 중입니다.</p>
-    </section>,
-    element,
+    </section>
   );
 }
 
