ed9325c77dc902264e021226887c8b1f8d89d04f
yousunzoo <dpfvmffjqm@gmail.com>
2024-03-30 15:29:52 +0900

    feat: 암장 정보 카드 클릭 시 해당 암장 상세 페이지로 이동

--- src/entities/map/ui/SelectedGymCard.tsx
+++ src/entities/map/ui/SelectedGymCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { GetBoulderingGymDetailResponse } from '../api/types';
 import SelectedGymContents from './SelectedGymContents';
@@ -12,25 +13,30 @@ const ENDPOINT = '/api/bouldering-gym/around';
 
 const SelectedGymCard = ({ id, onClose }: { id: number; onClose: () => void }) => {
   const { data, isLoading } = useSWR<GetBoulderingGymDetailResponse>(`${ENDPOINT}/${id}`);
+
+  const detailUrl = `/details/${id}`;
+
   return (
     <article className="fixed bottom-20 max-w-3xl left-0 pb-8 right-0 mx-auto w-full h-[232px] bg-neutral-800 rounded-t-[10px]">
       <button type="button" className="absolute top-5 right-5 text-[0px]" onClick={onClose}>
         <Icon name="Close" />
         닫기
       </button>
-      {isLoading ? (
-        <div className="flex flex-col justify-center items-center w-full h-full">
-          <Image
-            src={'/images/spinner.png'}
-            className="animate-spin"
-            width={50}
-            height={50}
-            alt="위치 정보 불러오는 중"
-          />
-        </div>
-      ) : (
-        <SelectedGymContents data={data} />
-      )}
+      <Link href={detailUrl}>
+        {isLoading ? (
+          <div className="flex flex-col justify-center items-center w-full h-full">
+            <Image
+              src={'/images/spinner.png'}
+              className="animate-spin"
+              width={50}
+              height={50}
+              alt="위치 정보 불러오는 중"
+            />
+          </div>
+        ) : (
+          <SelectedGymContents data={data} />
+        )}
+      </Link>
     </article>
   );
 };
