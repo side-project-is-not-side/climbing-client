04ad230b38f88263e3562973d2eefce7db564782
yousunzoo <dpfvmffjqm@gmail.com>
2024-03-30 15:15:35 +0900

    refactor: 섹터 업데이트 정보 컴포넌트 사용 통일

--- src/shared/ui/SectorUpdateInfo.tsx
+++ src/shared/ui/SectorUpdateInfo.tsx
@@ -3,26 +3,40 @@ import React from 'react';
 import { Icon } from '../icons';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
+import { twMerge } from 'tailwind-merge';
 
 import { TSectorUpdateInfo } from '@/entities/map/api/types';
 
 dayjs.locale('ko');
 const SectorUpdateInfo = ({
+  className,
   sectorUpdateInfo,
   type,
 }: {
+  className?: string;
   sectorUpdateInfo: TSectorUpdateInfo;
   type: 'upcoming' | 'lastUpdated';
 }) => {
   const { date, name } = sectorUpdateInfo;
+
+  const cn = twMerge(
+    'flex gap-[10px] text-[14px]',
+    type === 'upcoming' ? 'text-neutral-white' : 'text-neutral-400',
+    className,
+  );
+
   return (
-    <div className={`flex gap-[10px] ${type === 'upcoming' ? 'text-neutral-white' : 'text-neutral-400'} text-[14px]`}>
+    <div className={cn}>
       <Icon name={type === 'upcoming' ? 'RedStone' : 'GrayStone'} size="20" />
       <p>
         {type === 'lastUpdated' && '마지막 업데이트'} {dayjs(date).format('M월 DD일 a h:mm')}
       </p>
-      <span>&middot;</span>
-      <p>{name}</p>
+      {name && (
+        <>
+          <span>&middot;</span>
+          <p>{name}</p>
+        </>
+      )}
     </div>
   );
 };
