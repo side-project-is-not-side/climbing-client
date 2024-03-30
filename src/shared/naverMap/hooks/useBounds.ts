86df792e010046d10900fd37cf07f29a6bd1ee84
yousunzoo <dpfvmffjqm@gmail.com>
2024-03-30 16:18:12 +0900

    fix: debounce 로직 수정

---
+++ src/shared/naverMap/hooks/useBounds.ts
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { TNaverMap } from '../types';
+
+import { Bounds } from '@/entities/map/api/types';
+
+const DELAY = 500;
+
+export const useBounds = (map: TNaverMap | null) => {
+  const [bounds, setBounds] = useState<Bounds>();
+  const timer = useRef<ReturnType<typeof setTimeout>>();
+
+  const handleBoundChange = (bounds: naver.maps.Bounds) => {
+    // @ts-expect-error naver maps api가 제공하는 naver.maps.Bounds의 타입이 any이기 때문에 에러가 발생합니다.
+    const { _max, _min } = bounds;
+    const { _lat: maxLatitude, _lng: maxLongitude } = _max;
+    const { _lat: minLatitude, _lng: minLongitude } = _min;
+
+    setBounds({
+      maxLatitude: String(maxLatitude),
+      maxLongitude: String(maxLongitude),
+      minLatitude: String(minLatitude),
+      minLongitude: String(minLongitude),
+    });
+  };
+
+  const handleBoudnsChangeWithDebounce = (bounds: naver.maps.Bounds) => {
+    if (timer) {
+      clearTimeout(timer.current);
+    }
+
+    const newTimer = setTimeout(() => {
+      handleBoundChange(bounds);
+    }, DELAY);
+
+    timer.current = newTimer;
+  };
+
+  useEffect(() => {
+    if (map) {
+      const currentBounds = map.getBounds();
+      handleBoundChange(currentBounds);
+
+      naver.maps.Event.addListener(map, 'bounds_changed', handleBoudnsChangeWithDebounce);
+    }
+  }, [map]);
+
+  return bounds;
+};
--- src/shared/naverMap/hooks/useNaverMap.ts
+++ src/shared/naverMap/hooks/useNaverMap.ts
@@ -1,9 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 
+import { useBounds } from './useBounds';
 import { useGeoLocation } from './useGeolocation';
 
-import { Bounds } from '@/entities/map/api/types';
-import { useDebounce } from '@/shared/hooks/useDebounce';
 import { INITIAL_CENTER, ZOOM_LEVEL } from '@/shared/naverMap/constants';
 import { Coordinates, NaverMapOptions, TNaverMap } from '@/shared/naverMap/types';
 
@@ -26,8 +25,7 @@ const useNaverMap = ({
 }: Props) => {
   const mapElementRef = useRef<HTMLDivElement | null>(null);
   const [map, setMap] = useState<TNaverMap | null>(null);
-  const [bounds, setBounds] = useState<Bounds>();
-
+  const bounds = useBounds(map);
   const { location, reload, isGeolocationLoading } = useGeoLocation(geolocationEnabled);
 
   const initializeMap = () => {
@@ -59,30 +57,6 @@ const useNaverMap = ({
   useEffect(() => {
     if (location) panTo(location);
   }, [location]);
-
-  const handleBoundChange = (bounds: naver.maps.Bounds) => {
-    // @ts-expect-error naver maps api가 제공하는 naver.maps.Bounds의 타입이 any이기 때문에 에러가 발생합니다.
-    const { _max, _min } = bounds;
-    const { _lat: maxLatitude, _lng: maxLongitude } = _max;
-    const { _lat: minLatitude, _lng: minLongitude } = _min;
-
-    setBounds({
-      maxLatitude: String(maxLatitude),
-      maxLongitude: String(maxLongitude),
-      minLatitude: String(minLatitude),
-      minLongitude: String(minLongitude),
-    });
-  };
-
-  const handleBoundWithDebounce = useDebounce(() => handleBoundChange, 300);
-
-  useEffect(() => {
-    if (map) {
-      const currentBounds = map.getBounds();
-      handleBoundChange(currentBounds);
-      naver.maps.Event.addListener(map, 'bounds_changed', handleBoundWithDebounce);
-    }
-  }, [map]);
 
   return {
     mapElementRef,
