dba8e1290666f9698e3f2c34724f526abb52dec7
yousunzoo <dpfvmffjqm@gmail.com>
2024-03-30 19:14:24 +0900

    style: 메인 배너 비율 수정

--- src/entities/main/ui/Banner.tsx
+++ src/entities/main/ui/Banner.tsx
@@ -9,7 +9,7 @@ function Banner() {
       <h3 className="text-white font-bold text-lg mb-5">내 근처의 암장은?</h3>
 
       <Link href={'/map'}>
-        <div className="h-[210px] rounded-[10px]  sm:h-[420px] relative">
+        <div className="w-full relative aspect-[1.7]">
           <Image src="/images/main/banner.png" fill alt="다가오는 세팅" sizes="100%"></Image>
         </div>
       </Link>
