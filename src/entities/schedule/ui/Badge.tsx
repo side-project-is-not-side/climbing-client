8a7155230b3ae887c73706e3feabdb64da042e65
dkmqflx <dkmqflx@gmail.com>
2024-03-30 15:43:42 +0900

    feat: d day 보여주는Badge,  - 없는 곳 추가

--- src/entities/schedule/ui/Badge.tsx
+++ src/entities/schedule/ui/Badge.tsx
@@ -7,7 +7,7 @@ function Badge({ sector }: Pick<BoulderingGym, 'sector'>) {
   return (
     <div className="border-primary-400 border-[1px] right-[18px] top-[18px] absolute flex items-center justify-center bg-neutral-800 w-[40px] py-[2px] rounded-[6px]">
       <span className="text-neutral-white text-[12px]">
-        D{dayjs(sector.date).diff(new Date(), 'day') === 0 ? 'Day' : dayjs(sector.date).diff(new Date(), 'day')}
+        D-{dayjs(sector.date).diff(new Date(), 'day') === 0 ? 'Day' : dayjs(sector.date).diff(new Date(), 'day')}
       </span>
     </div>
   );
