204608b128e89160c0746f6d7c44707a0e5212ad
yousunzoo <dpfvmffjqm@gmail.com>
2024-06-05 22:54:22 +0900

    refactor: typography에 line-height 추가

--- src/shared/constants/typography.ts
+++ src/shared/constants/typography.ts
@@ -2,34 +2,42 @@ const typography = {
   '.font-display-1': {
     fontSize: '24px',
     fontWeight: 700,
+    lineHeight: '33px',
   },
   '.font-header-1': {
     fontSize: '18px',
     fontWeight: 700,
+    lineHeight: '26px',
   },
   '.font-header-2': {
     fontSize: '16px',
     fontWeight: 700,
+    lineHeight: '23px',
   },
   '.font-header-3': {
     fontSize: '14px',
     fontWeight: 700,
+    lineHeight: '20px',
   },
   '.font-text-1': {
     fontSize: '16px',
     fontWeight: 400,
+    lineHeight: '23px',
   },
   '.font-text-2': {
     fontSize: '14px',
     fontWeight: 400,
+    lineHeight: '20px',
   },
   '.font-flag': {
     fontSize: '12px',
     fontWeight: 700,
+    lineHeight: '17px',
   },
   '.font-bottom-tab': {
     fontSize: '10px',
     fontWeight: 400,
+    lineHeight: '14px',
   },
 } as const;
 
