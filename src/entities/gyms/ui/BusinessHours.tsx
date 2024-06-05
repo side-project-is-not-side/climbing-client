36fcd42d10f9adbfda2c64dfbdf5a64675b04252
yousunzoo <dpfvmffjqm@gmail.com>
2024-06-05 23:00:48 +0900

    style: 영업 시간 컴포넌트 퍼블리싱

---
+++ src/entities/gyms/ui/BusinessHours.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import type { BusinessHours } from '../api/type';
+
+import { ObjectTyped } from '@/shared/lib/typing';
+
+type Props = {
+  businessHours: BusinessHours;
+};
+
+const DAY_LABEL: Record<keyof BusinessHours, string> = {
+  monday: '월',
+  tuesday: '화',
+  wednesday: '수',
+  thursday: '목',
+  friday: '금',
+  saturday: '토',
+  sunday: '일',
+};
+
+function BusinessHours({ businessHours }: Props) {
+  return (
+    <dl className="flex flex-col flex-wrap w-full h-[92px] gap-1">
+      {ObjectTyped.entries(businessHours).map(([key, value]) => (
+        <div key={key} className="flex w-1/2 gap-[10px] leading-5">
+          <dt className="font-text-2 text-neutral-white">{DAY_LABEL[key]}</dt>
+          <dd className="font-text-2 text-neutral-400">{value}</dd>
+        </div>
+      ))}
+    </dl>
+  );
+}
+
+export default BusinessHours;
