88f10aa34cc4e2a329bdf0b55008e780f5c29596
yousunzoo <dpfvmffjqm@gmail.com>
2024-06-11 23:26:01 +0900

    refactor: 현재 선택된 암장 정보 가져오는 로직 쿼리 훅으로 분리

--- app/map/details/[slug]/page.tsx
+++ app/map/details/[slug]/page.tsx
@@ -2,21 +2,12 @@
 
 import React from 'react';
 
-import useSWR from 'swr';
-
-import { GetGymDetailResponse } from '@/entities/gyms/api/type';
 import GymDetailInfo from '@/entities/gyms/ui/GymDetailInfo';
-import { getUrlWithoutHost } from '@/shared/lib/getUrl';
-import { INITIAL_CENTER } from '@/shared/naverMap/constants';
-import { useGeoLocation } from '@/shared/naverMap/hooks/useGeolocation';
+import { useGetGymDetailInfo } from '@/features/gyms/queries/useGetGymDetailInfo';
 
 function DetailsPage({ params }: { params: { slug: string } }) {
   const id = params.slug;
-  const { location } = useGeoLocation(true);
-  const [latitude, longitude] = location ?? INITIAL_CENTER;
-
-  const url = getUrlWithoutHost(`/v1/gyms/${id}`, { latitude: String(latitude), longitude: String(longitude) });
-  const { data, isLoading } = useSWR<GetGymDetailResponse>(url);
+  const { data, isLoading } = useGetGymDetailInfo(Number(id));
 
   if (!data || isLoading) return null;
 
