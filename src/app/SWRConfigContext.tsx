e28f692fe02616d8d88fea0bcfd95cfba2209f09
yousunzoo <dpfvmffjqm@gmail.com>
2024-06-24 22:34:17 +0900

    test: 로그인 환경 API 테스트를 위한 토큰 설정

--- src/app/SWRConfigContext.tsx
+++ src/app/SWRConfigContext.tsx
@@ -10,7 +10,13 @@ function SWRConfigContext({ children }: Props) {
   return (
     <SWRConfig
       value={{
-        fetcher: (url: string) => fetch(`https://${process.env.NEXT_PUBLIC_API_HOST}${url}`).then((res) => res.json()),
+        fetcher: (url: string) =>
+          fetch(`https://${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+            headers: {
+              // TODO: Authorization 구현 후 연결 필요
+              Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwiaWF0IjoxNzE5MjE5NTUwLCJleHAiOjE3MTkyNTExMDd9.uPmJlC3i1qjJfUHpxMxjxviLZBhhdVVOJIxSbY5119c`,
+            },
+          }).then((res) => res.json()),
       }}
     >
       {children}
