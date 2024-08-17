b3db524e17b93088621ed99a7a2570407f4bc6cc
yousunzoo <dpfvmffjqm@gmail.com>
2024-08-17 18:14:02 +0900

    style: boxShadow 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,6 +49,11 @@ const config = {
       mPlus: ['var(--font-mPlus1p)'],
       cafeSurround: ['var(--font-cafeSurround)'],
     },
+    extend: {
+      boxShadow: {
+        custom: '0px 4px 16px 0px rgba(236, 230, 223, 0.25)',
+      },
+    },
   },
   plugins: [
     ({ addUtilities }) => {
