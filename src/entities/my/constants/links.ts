ae5a1fc79af9f3f9c9aa46b95992acc26ecf2bc5
yousunzoo <dpfvmffjqm@gmail.com>
2024-09-23 21:33:22 +0900

    feat: 외부 url 값 추가

---
+++ src/entities/my/constants/links.ts
@@ -0,0 +1,6 @@
+export const MYPAGE_LINKS = {
+  공지사항: 'https://phantom-lake-336.notion.site/1c81ec2d352e49edab1cfd79bfe836bd?pvs=74',
+  개인정보처리방침: 'https://phantom-lake-336.notion.site/6b575903cd994210b767d1eacb67c8c7',
+  이용약관: 'https://phantom-lake-336.notion.site/5267348ff1e94a7aab94e7caa1b2aeed',
+  문의: 'https://phantom-lake-336.notion.site/c1964a0c37d4464592dc8519f836d99d',
+} as const;
