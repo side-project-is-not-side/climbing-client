651daf16c12438fea27530b7f69c926ff11f54ca
yousunzoo <dpfvmffjqm@gmail.com>
2024-11-12 20:02:56 +0900

    refactor: 애플 탈퇴 버튼 클릭 시 앱으로 연결

--- src/features/auth/hooks/useWithdrawal.ts
+++ src/features/auth/hooks/useWithdrawal.ts
@@ -1,14 +1,19 @@
 'use client';
 
+import useSWR from 'swr';
 import useSWRMutation from 'swr/mutation';
 
 import { useModalRegister } from '@/app';
 import { useAuthContext } from '@/app/AuthContextProvider';
+import { UserInfoMain } from '@/entities/challenges';
 import { ModalKeys } from '@/shared/constants';
 
 export const useWithdrawal = () => {
   const { token, removeToken } = useAuthContext();
   const { close } = useModalRegister(ModalKeys.회원탈퇴);
+
+  const { data } = useSWR<UserInfoMain>('/v1/user-info');
+  const { provider } = data ?? {};
 
   const withdraw = async () => {
     await fetch(`https://${process.env.NEXT_PUBLIC_API_HOST}/v1/withdraw`, {
@@ -30,6 +35,12 @@ export const useWithdrawal = () => {
   });
 
   const handleWithdrawal = () => {
+    if (provider === 'APPLE') {
+      if (window.ReactNativeWebView) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ type: 'APPLE_WITHDRAW' }));
+      }
+      return;
+    }
     trigger();
   };
 
